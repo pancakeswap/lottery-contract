c63efcc1b117503f7f73978299ef7214af3a6dfb
Crab <36354694+self-coding-crab@users.noreply.github.com>
2021-02-12 19:11:19 -0500

    fix: Update constructor init

--- contracts/LotteryNFT.sol
+++ contracts/LotteryNFT.sol
@@ -14,7 +14,7 @@ contract LotteryNFT is ERC1155, Ownable {
     mapping (uint256 => uint256) public issueIndex;
     mapping (uint256 => bool) public claimInfo;
 
-    constructor() public ERC1155("random meta uri") {}
+    constructor() public ERC1155("") {}
 
     function newLotteryItem(address player, uint8[4] memory _lotteryNumbers, uint256 _amount, uint256 _issueIndex)
         public onlyOwner
