7de7c5228827184a4ad6aa15e3ac6c55503b6140
Veronica <veronica.21.coutts@gmail.com>
2021-02-24 16:37:04 +0200

    Added hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12"
+      },
+      {
+        version: "0.7.3"
+      }
+    ]
+  } 
+};
